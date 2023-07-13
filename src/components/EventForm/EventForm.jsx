0a3dae866edeb4f80dea2fae2045532f1c1f42ad
Alex Petit <alexpetit6@gmail.com>
2023-07-13 11:31:41 -0700

    isMeeting checkbox toggles form

--- src/components/EventForm/EventForm.jsx
+++ src/components/EventForm/EventForm.jsx
@@ -22,31 +22,43 @@ export default function EventForm() {
     setIsChecked(!isChecked)
   }
   return (
-    <Form className='event-form'>
-      <Form.Check // prettier-ignore
-        type="checkbox"
-        id="checkbox-default"
-        label="Is this a meeting?"
-        onChange={handleCheck}
-        checked={isChecked}
-      />
-      <Form.Group className="mb-3" controlId="eventForm.title">
-        <Form.Label>Event Title</Form.Label>
-        <Form.Control name='text' type="text" placeholder="Enter a Title" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="eventForm.photo">
-        <Form.Label>Image Url</Form.Label>
-        <Form.Control name='photo' type='text' placeholder='Paste in Image URL' />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="eventForm.description">
-        <Form.Label>Event Description</Form.Label>
-        <Form.Control name='description' as="textarea" rows={4} />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="eventForm.date">
-        <Form.Label>Image Url</Form.Label>
-        <Form.Control name='date' type='text' placeholder='Paste in Image URL' />
-      </Form.Group>
-    </Form>
+      isChecked
+      ?
+      <Form className='event-form'>
+        <Form.Check
+          type="checkbox"
+          id="checkbox-default"
+          label="Is this a meeting?"
+          onChange={handleCheck}
+          checked={isChecked}
+        />
+        <Form.Group className="mb-3" controlId="eventForm.date">
+          <Form.Label>Meeting Date</Form.Label>
+          <Form.Control name='date' type='date' placeholder='Enter Date' />
+        </Form.Group>
+      </Form>
+      :
+      <Form className='event-form'>
+        <Form.Check
+          type="checkbox"
+          id="checkbox-default"
+          label="Is this a meeting?"
+          onChange={handleCheck}
+          checked={isChecked}
+        />
+        <Form.Group className="mb-3" controlId="eventForm.title">
+          <Form.Label>Event Title</Form.Label>
+          <Form.Control name='text' type="text" placeholder="Enter a Title" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="eventForm.photo">
+          <Form.Label>Image Url</Form.Label>
+          <Form.Control name='photo' type='text' placeholder='Paste in Image URL' />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="eventForm.description">
+          <Form.Label>Event Description</Form.Label>
+          <Form.Control name='description' as="textarea" rows={4} />
+        </Form.Group>
+      </Form>
   )
 }
 
