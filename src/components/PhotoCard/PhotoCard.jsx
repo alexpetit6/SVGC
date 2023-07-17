01cadcdc880b618f7a9c210a118406a5faf2648e
Alex Petit <alexpetit6@gmail.com>
2023-07-17 15:11:12 -0700

    index works and photocard looks good

--- src/components/PhotoCard/PhotoCard.jsx
+++ src/components/PhotoCard/PhotoCard.jsx
@@ -1,3 +1,37 @@
-export default function PhotoCard() {
+import Card from 'react-bootstrap/Card'
+import Row  from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+export default function PhotoCard({ photos, i, url, title }) {
   
+  if (!i || i % 2) {
+    const next = photos[i + 1]
+    return (
+      <Row>
+        <Col md>
+          <Card>
+            <Card.Body>
+              <Card.Text className='text-center'>{title}</Card.Text>
+            </Card.Body>
+            <Card.Img variant="bottom" src={url} />
+          </Card>
+        </Col>
+        {
+        next
+        ?
+        <Col md>
+          <Card>
+            <Card.Body>
+              <Card.Text className='text-center'>{next.title}</Card.Text>
+            </Card.Body>
+            <Card.Img variant="bottom" src={next.url} />
+          </Card>
+        </Col>
+        :
+        <Col md></Col>
+        }
+      </Row>
+    );
+  };
+  return
 }
--- src/pages/PhotoGallery/PhotoGallery.jsx
+++ src/pages/PhotoGallery/PhotoGallery.jsx
@@ -17,9 +17,9 @@ export default function() {
   return (
     <>
       <PhotoForm setPhotos={setPhotos} photos={photos} />
-      {/* <Container>
-        {photos.map(p => <PhotoCard url={p.url} key={p._id} />)}
-      </Container> */}
+      <Container>
+        {photos.map((p, i) => <PhotoCard i={i} photos={photos} url={p.url} title={p.title} key={p._id} />)}
+      </Container>
     </>
   )
 }
