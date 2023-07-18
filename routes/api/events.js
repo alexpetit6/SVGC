8297ad8cdf21441f9eb4657083aaf13ac105b078
Alex Petit <alexpetit6@gmail.com>
2023-07-18 10:48:39 -0700

    added ensureLoggedIn to CUD routes

--- routes/api/events.js
+++ routes/api/events.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const eventsCtrl = require('../../controllers/api/events')
+const ensureLoggedIn = require('../../config/ensureLoggedIn')
 //All paths start with 'api/events'
 
 router.get('/', eventsCtrl.index);
 router.get('/:id', eventsCtrl.show);
-router.post('/', eventsCtrl.create);
-router.delete('/:id', eventsCtrl.delete);
-router.put('/:id', eventsCtrl.update)
+router.post('/', ensureLoggedIn, eventsCtrl.create);
+router.delete('/:id', ensureLoggedIn, eventsCtrl.delete);
+router.put('/:id', ensureLoggedIn, eventsCtrl.update)
 
 module.exports = router
--- routes/api/photos.js
+++ routes/api/photos.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const upload = require("multer")();
 const photosCtrl = require('../../controllers/api/photos');
-
+const ensureLoggedIn = require('../../config/ensureLoggedIn')
 
 router.get('/', photosCtrl.index);
-router.post('/upload', upload.single('photo'), photosCtrl.upload);
-router.delete('/:id', photosCtrl.delete);
+router.post('/upload', ensureLoggedIn, upload.single('photo'), photosCtrl.upload);
+router.delete('/:id', ensureLoggedIn, photosCtrl.delete);
 // The 'photo' maps to the name used when adding the input to the FormData object
 
 module.exports = router;
