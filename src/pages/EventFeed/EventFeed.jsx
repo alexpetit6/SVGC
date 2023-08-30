0e469cc701283be980db13a94a6ff5269ff8791a
Alex Petit <alexpetit6@gmail.com>
2023-08-29 19:29:28 -0700

    got rid of scrollY experiment that was accidentally saved

--- src/pages/EventFeed/EventFeed.jsx
+++ src/pages/EventFeed/EventFeed.jsx
@@ -5,7 +5,6 @@ import EventCard from '../../components/EventCard/EventCard'
 
 export default function EventFeed({user}) {
   const [events, setEvents] = useState([]);
-  const [scroll, setScroll] = useState(0);
   useEffect(function() {
     async function getAllEvents() {
       const allEvents = await getEvents();
@@ -13,17 +12,12 @@ export default function EventFeed({user}) {
     }
     getAllEvents();
   }, [])
-  function getScrollY() {
-    setScroll(window.scrollY)
-  }
-  window.addEventListener('scroll', getScrollY)
   const EventCards = events.map(e => <EventCard event={e} key={e._id} setEvents={setEvents} user={user}/>)
   return (
     <>
     <ListGroup>
       {EventCards}
     </ListGroup>
-    <h1> {scroll} </h1>  
     </>
   );
 }
