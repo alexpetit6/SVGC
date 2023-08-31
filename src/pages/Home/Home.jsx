199daec9534e3e183cbc37798c9f1b3be6f48d8f
Alex Petit <alexpetit6@gmail.com>
2023-08-31 16:05:10 -0700

    made middle portion of home layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,13 +8,23 @@ export default function Home() {
       <h1 id='brand-statement' className='text-center'>Brand Statement</h1>
     </div>
     <Row>
-      <Col>
-        <div className='square-content'>
+      <Col xl className='text-col'>
+        <div className='img-text'>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quasi, nihil rem tempora est sequi doloremque debitis aliquam pariatur consequuntur aliquid aspernatur illo adipisci voluptates animi velit doloribus, reprehenderit odio!</p>
         </div>
       </Col>
-      <Col>
-
+      <Col className='home-pix' xl>
+        <img className='img-fluid' id='home-pic-1' src="https://i.imgur.com/e09OXu0.jpg" alt="" />
+      </Col>
+    </Row>
+    <Row>
+      <Col className='home-pix' xl>
+        <img className='img-fluid' id='home-pic-1' src="https://i.imgur.com/e09OXu0.jpg" alt="" />
+      </Col>
+      <Col xl className='text-col'>
+        <div className='img-text'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quasi, nihil rem tempora est sequi doloremque debitis aliquam pariatur consequuntur aliquid aspernatur illo adipisci voluptates animi velit doloribus, reprehenderit odio!</p>
+        </div>
       </Col>
     </Row>
     </>
