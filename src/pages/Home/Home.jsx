650c43356f7fc9cb78a84fbd480cb43ee8efde6e
Alex Petit <alexpetit6@gmail.com>
2023-08-31 09:38:00 -0700

    made navbar absolute

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,16 @@ export default function Home() {
     <div id='home-img'>
       <h1 id='brand-statement' className='text-center'>Brand Statement</h1>
     </div>
+    <Row>
+      <Col>
+        <div className='square-content'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quasi, nihil rem tempora est sequi doloremque debitis aliquam pariatur consequuntur aliquid aspernatur illo adipisci voluptates animi velit doloribus, reprehenderit odio!</p>
+        </div>
+      </Col>
+      <Col>
+
+      </Col>
+    </Row>
     </>
   )
 }
