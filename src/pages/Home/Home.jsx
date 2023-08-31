41e53cc895e999fa610ae4a09ab942a88ba85571
Alex Petit <alexpetit6@gmail.com>
2023-08-31 09:18:13 -0700

    Made home-img

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import './Home.css'
+import { Row, Col, Card } from 'react-bootstrap'
 
 export default function Home() {
   return (
     <>
-    <div className='img-crop'>
-      <img className="img-fluid header-img" src="https://i.imgur.com/6FuGBua.jpg" alt="" />
+    <div id='home-img'>
+      <h1 id='brand-statement' className='text-center'>Brand Statement</h1>
     </div>
     </>
   )
