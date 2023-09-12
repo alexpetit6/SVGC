69925ea230d4abd4b143c1a4f2afc645d532d99a
Alex Petit <alexpetit6@gmail.com>
2023-09-12 11:46:23 -0700

    made event cards even more responsive and fixed admin buttons

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -20,17 +20,17 @@ export default function EventCard({ event, setEvents, user }) {
             <h1>{event.displayDate.slice(0, 4)}</h1>
             <h3>{event.displayDate.slice(4)}</h3>
           </div>
-          <Card.Text>{event.standardClock}</Card.Text>
-          <Card.Text>
+          <Card.Text className='time-place'>{event.standardClock}</Card.Text>
+          <Card.Text className='time-place place'>
             <a href={`https://www.google.com/maps/place/${encodeURI(event.location)}`} target='_blank' rel='noreferrer'>{event.location}</a>
           </Card.Text>
           {
             user
             ?
-            <>
-            <ActionButton action='deleteEvt' id={event.id} setterFunc={setEvents} />
-            <Button className='action-button' variant='warning' size='sm'><Link to={`/events/new/${event.id}`}>EDIT</Link></Button>
-            </>
+            <div className='event-buttons'>
+              <ActionButton action='deleteEvt' id={event.id} setterFunc={setEvents} />
+              <Button variant='warning' size='sm'><Link to={`/events/new/${event.id}`}>EDIT</Link></Button>
+            </div>
             :
             null
           }
