d32a28dbe24205d10324fed49031e87032908ee6
Alex Petit <alexpetit6@gmail.com>
2023-09-29 17:59:10 -0700

    community page retrieves data

--- src/pages/CommunityPage/CommunityPage.jsx
+++ src/pages/CommunityPage/CommunityPage.jsx
@@ -1,57 +1,61 @@
 import './CommunityPage.css';
 import { Button } from 'react-bootstrap';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CommunityForm from '../../components/CommunityForm/CommunityForm';
+import { getCommunity } from '../../utilities/community-api';
 
 export default function CommunityPage({ user }) {
+  const [community, setCommunity] = useState();
   const [editing, setEditing] = useState(false);
+
+  useEffect(function() {
+    async function getDetails() {
+      const community = await getCommunity();
+      setCommunity(community);
+    }
+    getDetails();
+  }, []);
 
   function handleEditing() {
     setEditing(!editing);
   }
 
-  return (
-    editing
-    ?
-    <>
-    <div className="header-img" >
-      <img src="https://i.imgur.com/zcyoc9z.jpg)" alt="" />
-      <h1 className='header-text'>Community Service</h1>
-    </div>
-    { user ? <Button id='edit-community-btn' onClick={handleEditing} variant='warning' size='lg'>Stop Editing</Button> : null }
-    <CommunityForm />
-    </>
-    :
-    <>
-    <div className="header-img" >
-      <img src="https://i.imgur.com/zcyoc9z.jpg)" alt="" />
-      <h1 className='header-text'>Community Service</h1>
-    </div>
-    { user ? <Button id='edit-community-btn' onClick={handleEditing} variant='warning' size='lg'>EDIT</Button> : null }
-    <div id='community-body'>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque est, voluptatem assumenda ad sequi soluta ducimus sit alias quia deserunt aperiam perferendis omnis molestiae! Hic architecto voluptatibus ullam facilis cumque.
-        Sed vel quam in placeat quos exercitationem officiis doloremque earum nostrum minus sequi obcaecati minima inventore hic atque dolorem doloribus deleniti, maxime vero. Iusto hic optio officiis! Architecto, veniam itaque.
-        Eaque corrupti animi labore voluptatem id maxime doloribus commodi amet odio tempora? Esse ad sapiente quam animi, amet voluptas nulla, repudiandae ullam provident corporis id eligendi autem illo quibusdam optio.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque est, voluptatem assumenda ad sequi soluta ducimus sit alias quia deserunt aperiam perferendis omnis molestiae! Hic architecto voluptatibus ullam facilis cumque.
-        Sed vel quam in placeat quos exercitationem officiis doloremque earum nostrum minus sequi obcaecati minima inventore hic atque dolorem doloribus deleniti, maxime vero. Iusto hic optio officiis! Architecto, veniam itaque.
-        Eaque corrupti animi labore voluptatem id maxime doloribus commodi amet odio tempora? Esse ad sapiente quam animi, amet voluptas nulla, repudiandae ullam provident corporis id eligendi autem illo quibusdam optio.
-      </p>
-      <img src="https://i.imgur.com/IatMLtr.jpg" alt="" />
-      <img src="https://i.imgur.com/IatMLtr.jpg" alt="" />
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque est, voluptatem assumenda ad sequi soluta ducimus sit alias quia deserunt aperiam perferendis omnis molestiae! Hic architecto voluptatibus ullam facilis cumque.
-        Sed vel quam in placeat quos exercitationem officiis doloremque earum nostrum minus sequi obcaecati minima inventore hic atque dolorem doloribus deleniti, maxime vero. Iusto hic optio officiis! Architecto, veniam itaque.
-        Eaque corrupti animi labore voluptatem id maxime doloribus commodi amet odio tempora? Esse ad sapiente quam animi, amet voluptas nulla, repudiandae ullam provident corporis id eligendi autem illo quibusdam optio.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque est, voluptatem assumenda ad sequi soluta ducimus sit alias quia deserunt aperiam perferendis omnis molestiae! Hic architecto voluptatibus ullam facilis cumque.
-        Sed vel quam in placeat quos exercitationem officiis doloremque earum nostrum minus sequi obcaecati minima inventore hic atque dolorem doloribus deleniti, maxime vero. Iusto hic optio officiis! Architecto, veniam itaque.
-        Eaque corrupti animi labore voluptatem id maxime doloribus commodi amet odio tempora? Esse ad sapiente quam animi, amet voluptas nulla, repudiandae ullam provident corporis id eligendi autem illo quibusdam optio.
-      </p>
-    </div>
-    </>
-  )
+  if (community) {
+    return (
+      editing
+      ?
+      <>
+      <div className="header-img" >
+        <img src="https://i.imgur.com/zcyoc9z.jpg)" alt="" />
+        <h1 className='header-text'>Community Service</h1>
+      </div>
+      { user ? <Button id='edit-community-btn' onClick={handleEditing} variant='warning' size='lg'>Stop Editing</Button> : null }
+      <CommunityForm />
+      </>
+      :
+      <>
+      <div className="header-img" >
+        <img src="https://i.imgur.com/zcyoc9z.jpg)" alt="" />
+        <h1 className='header-text'>Community Service</h1>
+      </div>
+      { user ? <Button id='edit-community-btn' onClick={handleEditing} variant='warning' size='lg'>EDIT</Button> : null }
+      <div id='community-body'>
+        <p>
+          {community.intro}
+        </p>
+        <p>
+          {community.body1}
+        </p>
+        <img src={community.img1} alt="" />
+        <img src={community.img2} alt="" />
+        <p>
+          {community.body2}
+        </p>
+        <p>
+          {community.outro}
+        </p>
+      </div>
+      </>
+    )
+  }
 }
